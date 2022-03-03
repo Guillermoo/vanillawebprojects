08c5dfdd3d72818758f6ae95a149a8dcd6e59b2e
Guillermoo <guillermo.cano.es@gmail.com>
2022-03-03 23:36:47 +0100

    #Form Validator, checkLenght

--- form-validator/script.js
+++ form-validator/script.js
@@ -1,86 +1,52 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+'use strict';
 
-// Show input error message
-function showError(input, message) {
-  const formControl = input.parentElement;
-  formControl.className = 'form-control error';
-  const small = formControl.querySelector('small');
-  small.innerText = message;
-}
+const myForm = document.querySelector('.form');
 
-// Show success outline
-function showSuccess(input) {
-  const formControl = input.parentElement;
-  formControl.className = 'form-control success';
-}
-
-// Check email is valid
-function checkEmail(input) {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (re.test(input.value.trim())) {
-    showSuccess(input);
-  } else {
-    showError(input, 'Email is not valid');
-  }
-}
-
-// Check required fields
-function checkRequired(inputArr) {
-  let isRequired = false;
-  inputArr.forEach(function(input) {
-    if (input.value.trim() === '') {
-      showError(input, `${getFieldName(input)} is required`);
-      isRequired = true;
-    } else {
-      showSuccess(input);
-    }
-  });
-
-  return isRequired;
-}
-
-// Check input length
-function checkLength(input, min, max) {
-  if (input.value.length < min) {
-    showError(
-      input,
-      `${getFieldName(input)} must be at least ${min} characters`
-    );
-  } else if (input.value.length > max) {
-    showError(
-      input,
-      `${getFieldName(input)} must be less than ${max} characters`
-    );
-  } else {
-    showSuccess(input);
-  }
-}
-
-// Check passwords match
-function checkPasswordsMatch(input1, input2) {
-  if (input1.value !== input2.value) {
-    showError(input2, 'Passwords do not match');
-  }
-}
-
-// Get fieldname
-function getFieldName(input) {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-}
-
-// Event listeners
-form.addEventListener('submit', function(e) {
+myForm.addEventListener('submit', function (e) {
   e.preventDefault();
+  const myFields = e.target.querySelectorAll('.form-control');
 
-  if(checkRequired([username, email, password, password2])){
-    checkLength(username, 3, 15);
-    checkLength(password, 6, 25);
-    checkEmail(email);
-    checkPasswordsMatch(password, password2);
+  //console.log(myFields);
+
+  myFields.forEach((field) => {
+    const inputField = field.getElementsByTagName('input');
+    if (!checkLength(inputField[0].id, inputField[0].value))
+      showMessage('error', inputField[0].id);
+    else console.log(`Pasamos ${inputField[0].value}`);
+  });
+  //   for (let i = 0; i < inputs.length; i++) {
+  //     const input = inputs[i];
+  //     checkRequired(input.value);
+  //   }
+});
+
+// check if empty string
+// function checkRequired(inputField) {
+//   if (inputField[0].value) return true;
+//   return false;
+// }
+
+function checkLength(field, strng) {
+  let minChar = 0;
+  let maxChar = 0;
+  const stringSize = strng.length;
+
+  if (field === 'password2' || field === 'email') return true;
+
+  if (field === 'username') {
+    minChar = 3;
+    maxChar = 15;
+  }
+  if (field === 'password') {
+    minChar = 6;
+    maxChar = 25;
   }
 
-});
+  if (minChar < stringSize && stringSize < maxChar) return true;
+
+  return false;
+}
+
+function showMessage(msg, sel) {
+  console.log(`${msg}, ${sel}`);
+}
