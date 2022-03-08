0d9c4937db3d140a1e1beeba7e57abe77778a822
Guillermoo <guillermo.cano.es@gmail.com>
2022-03-08 23:27:02 +0100

    custom-video progress-bar2

--- custom-video-player/script.js
+++ custom-video-player/script.js
@@ -5,47 +5,81 @@ const allBtns = document.querySelectorAll('.btn');
 const playBtn = document.getElementById('play');
 const stopBtn = document.getElementById('stop');
 const videoPlayer = document.getElementById('video');
+const timeStmp = document.getElementById('timestamp');
+const slider = document.getElementById('progress');
+
+let interval;
 
 allCtrls.addEventListener('click', function (e) {
   e.preventDefault();
 
-  console.log(e.target);
   handleButtons(this, e.target);
 });
 
-// allBtns.forEach(function (btn) {
-//   btn.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     handleButtons(e.target);
-//   });
-// });
+videoPlayer.addEventListener('click', function (e) {
+  e.preventDefault();
 
-// playBtn.addEventListener('click', function (e) {
-//   e.preventDefault();
-//   //console.log(e.target);
+  allCtrls.querySelector('#play > i').classList.remove('fa-play');
+  allCtrls.querySelector('#play > i').classList.add('fa-pause');
+  startInterval();
+  videoPlayer.play();
+});
 
-//   handleButtons(e.target);
-// });
+function minTommss(minutes) {
+  var sign = minutes < 0 ? '-' : '';
 
-// stopBtn.addEventListener('click', function (e) {
-//   handleButtons(e.target);
-// });
+  var min = Math.floor(Math.abs(minutes) / 60);
+  var sec = Math.floor(minutes - min * 60);
+  var x = min < 10 ? '0' + min : min;
+  var y = sec < 10 ? '0' + sec : sec;
+  return x + ':' + y;
+}
+
+function startInterval() {
+  slider.max = video.duration;
+  interval = setInterval(() => {
+    timeStmp.innerHTML = minTommss(video.currentTime);
+    slider.value = video.currentTime;
+    if (slider.value == video.duration) console.log('finnn');
+  }, 10);
+}
+
+function stopInvertal(interval) {
+  clearInterval(interval);
+  timeStmp.innerHTML = '00:00';
+}
+function pauseInvertal(time) {
+  console.log(interval);
+  clearInterval(interval);
+  interval = time;
+}
 
 function handleButtons(container, btn) {
+  //console.log(btn);
   if (btn.classList.contains('fa-play')) {
     btn.classList.remove('fa-play');
     btn.classList.add('fa-pause');
+    startInterval();
     videoPlayer.play();
   } else if (btn.classList.contains('fa-pause')) {
     btn.classList.add('fa-play');
     btn.classList.remove('fa-pause');
     videoPlayer.pause();
+    pauseInvertal(videoPlayer.currentTime);
   } else if (btn.classList.contains('fa-stop')) {
     container.querySelector('#play > i').classList.add('fa-play');
     container.querySelector('#play > i').classList.remove('fa-pause');
     videoPlayer.pause();
+    stopInvertal(interval);
     videoPlayer.currentTime = 0;
+    slider.value = 0;
   } else if (btn.classList.contains('progress')) {
-    console.log('progress');
+    pauseInvertal(videoPlayer.currentTime);
+
+    console.log(btn);
+    console.log(btn.value);
+    //video.currentTime = btn.value;
+    timeStmp.innerHTML = minTommss(video.currentTime);
+    //startInterval();
   }
 }
