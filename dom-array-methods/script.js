7be38438e056a09ec369419d3eb2400788ea17c2
Guillermoo <guillermo.cano.es@gmail.com>
2022-03-10 19:44:09 +0100

    DOM Array Init

--- dom-array-methods/script.js
+++ dom-array-methods/script.js
@@ -1,96 +1,60 @@
-const main = document.getElementById('main');
-const addUserBtn = document.getElementById('add-user');
-const doubleBtn = document.getElementById('double');
-const showMillionairesBtn = document.getElementById('show-millionaires');
-const sortBtn = document.getElementById('sort');
-const calculateWealthBtn = document.getElementById('calculate-wealth');
+const MAX_WEALTH = 1000000;
+const MIN_WEALTH = 200000;
 
-let data = [];
+const persons = [];
 
-getRandomUser();
-getRandomUser();
-getRandomUser();
+const getDataAPI = async function () {
+  try {
+    const repUrl = `https://randomuser.me/api/`;
 
-// Fetch random user and add money
-async function getRandomUser() {
-  const res = await fetch('https://randomuser.me/api');
-  const data = await res.json();
+    fetch(repUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const { title, first, last } = data['results'][0]['name'];
+        console.log(title, first, last);
+      });
+  } catch {}
+};
 
-  const user = data.results[0];
+const formatter = new Intl.NumberFormat('es-ES', {
+  style: 'currency',
+  currency: 'EUR',
+});
 
-  const newUser = {
-    name: `${user.name.first} ${user.name.last}`,
-    money: Math.floor(Math.random() * 1000000)
-  };
-
-  addData(newUser);
+function createRandomWealth() {
+  return Math.floor(Math.random() * (MAX_WEALTH - MIN_WEALTH + 1) + MIN_WEALTH);
 }
 
-// Double eveyones money
-function doubleMoney() {
-  data = data.map(user => {
-    return { ...user, money: user.money * 2 };
-  });
+async function getNewPerson() {
+  try {
+    const repUrl = `https://randomuser.me/api/`;
 
-  updateDOM();
+    fetch(repUrl)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        const { title, first, last } = data['results'][0]['name'];
+
+        console.log(data['results'][0]['name']);
+        const person = {
+          title: title,
+          first: first,
+          last: last,
+          wealth: formatter.format(createRandomWealth()),
+        };
+
+        persons.push(person);
+        console.log(persons);
+      });
+  } catch {}
 }
 
-// Sort users by richest
-function sortByRichest() {
-  console.log(123);
-  data.sort((a, b) => b.money - a.money);
+function init() {
+  getNewPerson();
 
-  updateDOM();
+  console.log(persons);
 }
 
-// Filter only millionaires
-function showMillionaires() {
-  data = data.filter(user => user.money > 1000000);
-
-  updateDOM();
-}
-
-// Calculate the total wealth
-function calculateWealth() {
-  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
-
-  const wealthEl = document.createElement('div');
-  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
-    wealth
-  )}</strong></h3>`;
-  main.appendChild(wealthEl);
-}
-
-// Add new obj to data arr
-function addData(obj) {
-  data.push(obj);
-
-  updateDOM();
-}
-
-// Update DOM
-function updateDOM(providedData = data) {
-  // Clear main div
-  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
-
-  providedData.forEach(item => {
-    const element = document.createElement('div');
-    element.classList.add('person');
-    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
-      item.money
-    )}`;
-    main.appendChild(element);
-  });
-}
-
-// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
-function formatMoney(number) {
-  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-}
-
-// Event listeners
-addUserBtn.addEventListener('click', getRandomUser);
-doubleBtn.addEventListener('click', doubleMoney);
-sortBtn.addEventListener('click', sortByRichest);
-showMillionairesBtn.addEventListener('click', showMillionaires);
-calculateWealthBtn.addEventListener('click', calculateWealth);
+init();
