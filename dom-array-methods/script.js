7499abf83bb434c0e1671574243b851ea12f7e90
Guillermoo <guillermo.cano.es@gmail.com>
2022-03-21 18:44:21 +0100

    Dom-array Double final

--- dom-array-methods/script.js
+++ dom-array-methods/script.js
@@ -2,155 +2,132 @@ const MAX_WEALTH = 1000000;
 const MIN_WEALTH = 200000;
 
 const mainTable = document.getElementById('main');
+
 const addUserBtn = document.getElementById('add-user');
 const doubleMoneyBtn = document.getElementById('double');
+const showMillionairsBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calcWealth = document.getElementById('calculate-wealth');
 
 let persons = [];
 
 addUserBtn.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const newPerson = getNewPerson();
-  newPerson.then((person) => {
-    printPerson(person);
-  });
+  getRandomPersons(1);
 });
 
 doubleMoneyBtn.addEventListener('click', function (e) {
   e.preventDefault();
-  console.log('double');
+  persons = persons.map((person) => {
+    //Devuelvo la persona en array
+    return { ...person, wealth: person.wealth * 2 };
+  });
+
+  clearList();
+  printPersons(persons);
 });
 
-const getDataAPI = async function () {
-  try {
-    const repUrl = `https://randomuser.me/api/`;
+showMillionairsBtn.addEventListener('click', function (e) {
+  e.preventDefault();
 
-    fetch(repUrl)
-      .then((res) => res.json())
-      .then((data) => {
-        const { first, last } = data['results'][0]['name'];
-      });
-  } catch {}
-};
+  if (!persons) return '';
 
-const formatter = new Intl.NumberFormat('es-ES', {
-  style: 'currency',
-  currency: 'EUR',
+  persons = persons.filter((person) => person.wealth > 1000000);
+  printPersons(persons);
 });
+
+sortBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (!persons) return '';
+
+  persons = persons.sort(function (a, b) {
+    return b.wealth - a.wealth;
+  });
+
+  printPersons();
+});
+
+calcWealth.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  let sumWealth = persons.reduce((a, b) => {
+    return a + b.wealth;
+  }, 0);
+
+  const htmlWealth = `<div><h3>Total Wealth: <strong>${formatMoney(
+    sumWealth
+  )}</strong></h3></div>`;
+
+  mainTable.insertAdjacentHTML('beforeend', htmlWealth);
+});
+
+function clearList() {
+  mainTable.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+}
+
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
 
 function createRandomWealth() {
-  return Math.floor(Math.random() * (MAX_WEALTH - MIN_WEALTH + 1) + MIN_WEALTH);
+  return Math.floor(Math.random() * 1000000);
 }
 
 function addPerson(person) {
   persons.push(person);
 }
 
-function getRandomPersons() {
+async function getRandomPersons(number = 1) {
   try {
-    const repUrl = `https://randomuser.me/api/?results=5&?exc=gender,location,email,login,registered,dob,phone,cell,id,picture,nat`;
+    const res = await fetch(
+      `https://randomuser.me/api/?results=${number}&?exc=gender,location,email,login,registered,dob,phone,cell,id,picture,nat`
+    );
+    const data = await res.json();
 
-    fetch(repUrl)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        data['results'].forEach((person) => {
-          const { first, last } = person['name'];
-          const newperson = {
-            first: first,
-            last: last,
-            wealth: formatter.format(createRandomWealth()),
-          };
+    data.results.forEach((person) => {
+      const newperson = {
+        first: person['name'].first,
+        last: person['name'].last,
+        wealth: createRandomWealth(),
+      };
 
-          addPerson(newperson);
-          //printPerson(person);
-        });
-      });
+      addPerson(newperson);
+    });
+    //clearList()
+    printPersons();
   } catch (err) {
     console.log(err);
   }
 }
 
-async function getNewPerson() {
-  try {
-    const repUrl = `https://randomuser.me/api/?exc=gender,location,email,login,registered,dob,phone,cell,id,picture,nat`;
+function printPersons(myPersons = persons) {
+  //console.log(myPersons.length);
+  clearList();
 
-    const res = await fetch(repUrl);
-    const data = await res.json();
-    const user = data.results[0];
-    const person = {
-      first: user['name']['first'],
-      last: user['name']['last'],
-      wealth: formatter.format(createRandomWealth()),
-    };
-
-    persons.push(person);
-
-    return person;
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-function printPerson(person) {
-  const personDiv = document.createElement('div');
-  personDiv.classList.add('person');
-
-  const markUp = `
-    <strong>${person['first']} ${person['last']}</strong> ${person['wealth']}
-    `;
-
-  personDiv.innerHTML = markUp;
-  mainTable.appendChild(personDiv);
-}
-
-function printPersons(myPersons) {
-  console.log(myPersons);
-  console.log(typeof myPersons);
+  if (!myPersons) return '';
 
   myPersons.forEach((person) => {
-    console.log(person);
     const personDiv = document.createElement('div');
     personDiv.classList.add('person');
 
     const markUp = `
-    <strong>${person['title']} ${person['first']} ${person['last']}</strong> ${person['wealth']}
-    `;
+    <strong>${person['first']} ${person['last']}</strong>${formatMoney(
+      person['wealth']
+    )}`;
 
     personDiv.innerHTML = markUp;
     mainTable.appendChild(personDiv);
-    console.log(markUp);
-    console.log(personDiv);
+    // console.log(markUp);
+    // console.log(personDiv);
   });
-  console.log(myPersons);
 }
-
-/* function createPerson(person) {
-  console.log(person);
-  const personDiv = document.createElement('div');
-  personDiv.classList.add('person');
-
-  const markUp = `
-  <strong>${person['title']} ${person['first']} ${person['last']}</strong> ${person['wealth']}
-  `;
-
-  personDiv.innerHTML = markUp;
-  mainTable.appendChild(personDiv);
-  console.log(markUp);
-  console.log(personDiv);
-} */
 
 function init() {
-  //getNewPerson();
-  getRandomPersons();
+  getRandomPersons(3);
 
-  const mypersons = [
-    { first: 'Tamara', last: 'Berry', wealth: '366.170,00 €' },
-    { first: 'Alba', last: 'Gil', wealth: '872.134,00 €' },
-  ];
-
-  printPersons(persons);
+  printPersons();
 }
 
-//init();
+init();
