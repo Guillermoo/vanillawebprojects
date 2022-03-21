b62d0031c83bfdd8ab46f783da69d4b4bc27ddb7
Guillermoo <guillermo.cano.es@gmail.com>
2022-03-21 19:25:39 +0100

    modal menu

--- modal-menu-slider/script.js
+++ modal-menu-slider/script.js
@@ -1,40 +1,28 @@
-const toggle = document.getElementById('toggle');
-const close = document.getElementById('close');
-const open = document.getElementById('open');
-const modal = document.getElementById('modal');
-const navbar = document.getElementById('navbar');
+'use strict';
 
-// This function closes navbar if user clicks anywhere outside of navbar once it's opened
-// Does not leave unused event listeners on
-// It's messy, but it works
-function closeNavbar(e) {
-  if (
-    document.body.classList.contains('show-nav') &&
-    e.target !== toggle &&
-    !toggle.contains(e.target) &&
-    e.target !== navbar &&
-    !navbar.contains(e.target)
-  ) {
-    document.body.classList.toggle('show-nav');
-    document.body.removeEventListener('click', closeNavbar);
-  } else if (!document.body.classList.contains('show-nav')) {
-    document.body.removeEventListener('click', closeNavbar);
-  }
-}
+const btnToggle = document.getElementById('toggle');
+const btnOpen = document.getElementById('open');
+const navBar = document.getElementById('navbar');
+const modalContainer = document.getElementById('modal');
+const btnCloseModal = document.getElementById('close');
 
-// Toggle nav
-toggle.addEventListener('click', () => {
-  document.body.classList.toggle('show-nav');
-  document.body.addEventListener('click', closeNavbar);
-});
+btnToggle.addEventListener('click', () =>
+  document.body.classList.toggle('show-nav')
+);
 
-// Show modal
-open.addEventListener('click', () => modal.classList.add('show-modal'));
+// Show modal when clck SignUp
+btnOpen.addEventListener('click', () =>
+  modalContainer.classList.toggle('show-modal')
+);
 
-// Hide modal
-close.addEventListener('click', () => modal.classList.remove('show-modal'));
+// Close modal when click button close
+btnCloseModal.addEventListener('click', () =>
+  modalContainer.classList.toggle('show-modal')
+);
 
-// Hide modal on outside click
-window.addEventListener('click', e =>
-  e.target == modal ? modal.classList.remove('show-modal') : false
+//Hide modal when click out modal
+modalContainer.addEventListener('click', (e) =>
+  e.target === modalContainer
+    ? modalContainer.classList.toggle('show-modal')
+    : false
 );
