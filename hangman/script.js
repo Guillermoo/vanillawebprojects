9c0008c9db087f2b14ad741c4f18a8a86a142744
Guillermoo <guillermo.cano.es@gmail.com>
2022-03-22 22:59:16 +0100

    hangout finish

--- hangman/script.js
+++ hangman/script.js
@@ -3,9 +3,12 @@ const mylines = document.querySelectorAll('line.figure-part');
 const wordContainer = document.getElementById('word');
 const wrongLetterContainer = document.getElementById('wrong-letters');
 const notificationContainer = document.getElementById('notification-container');
+const popupContainer = document.querySelector('.popup-container');
+const btnPlayAgain = document.getElementById('play-button');
 
 let myWord = '';
 let lettersFailed = [];
+let lettersRight = [];
 
 //myhead.style.display = 'block';
 
@@ -17,46 +20,78 @@ window.addEventListener('keypress', function (e) {
   }
 });
 
-document.addEventListener('DOMContentLoaded', function (e) {
-  // e.preventDefault();
-  //getRandomWord();
-  myWord = 'tiburon';
+btnPlayAgain.addEventListener('click', function () {
+  cleanUI();
+  startGame();
+});
 
-  setLetters(myWord);
+function startGame() {
+  lettersFailed = [];
+  const newword = getRandomWord();
+
+  newword.then((theword) => {
+    myWord = theword;
+    // Set underscore
+    setLetters(myWord);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  startGame();
 });
 
 function checkLetter(letter) {
-  console.log(letter);
   if (!myWord) return '';
 
   const arrayWord = [...myWord];
-  //   console.log(myWord);
-  //   console.log(arrayWord);
-
-  if (arrayWord.includes(letter)) showRightLetter(letter);
-  else showWrongLetter(letter);
+  if (arrayWord.includes(letter)) manageRightLetter(letter);
+  else manageWrongLetter(letter);
 }
 
-function repeatedWrongLetter(letter) {
-  notificationContainer.classList.add('show');
-  setTimeout(notificationContainer.classList.remove('show'), 3000);
+function manageRightLetter(letter) {
+  isAlready(lettersRight, letter)
+    ? notifyRepeatedLetter(letter)
+    : showRightLetter(letter);
+
+  const wordGuessed = [...lettersRight];
+  console.log(wordGuessed);
+  console.log(myWord);
+
+  wordGuessed.join('') === myWord
+    ? showPopup('Congratulations! You won! 😃')
+    : '';
 }
 
-function showRightLetter(letter) {
-  console.log(`Right ${letter}`);
+function manageWrongLetter(letter) {
+  isAlready(lettersFailed, letter)
+    ? notifyRepeatedLetter(letter)
+    : showWrongLetter(letter);
+
+  manageStick(lettersFailed.length);
 }
-function showWrongLetter(letter) {
-  if (lettersFailed.includes(letter)) {
-    repeatedWrongLetter(letter);
-    return '';
+
+function notifyRepeatedLetter() {
+  notificationContainer.classList.toggle('show');
+  setTimeout(function () {
+    notificationContainer.classList.toggle('show');
+  }, 2200);
+}
+
+// Wrong Letter already typed
+function isAlready(alreadyletters, letter) {
+  if (alreadyletters.includes(letter)) {
+    return true;
   }
+  return false;
+}
 
+function showWrongLetter(letter) {
+  //If first letter
   if (wrongLetterContainer.childNodes.length < 1) {
     const pEl = document.createElement('p');
     pEl.innerHTML = 'Wrong';
     wrongLetterContainer.appendChild(pEl);
   }
-  console.log(wrongLetterContainer.childNodes);
 
   let comaChar = '';
   if (wrongLetterContainer.childNodes.length > 1) {
@@ -69,10 +104,46 @@ function showWrongLetter(letter) {
   lettersFailed.push(letter);
 }
 
+function showRightLetter(letter) {
+  wordContainer.childNodes[myWord.indexOf(letter)].innerHTML = letter;
+  lettersRight.push(letter);
+}
+
+function manageStick(count) {
+  //Show head
+  count === 1 ? (myhead.style.display = 'block') : '';
+
+  //You reach the max
+  count === 6 ? showPopup('Unfortunately you lost. 😕') : '';
+
+  //Add stick
+  count > 1 ? (mylines[count - 2].style.display = 'block') : '';
+}
+
+function hidePopup() {
+  popupContainer.style.display = 'none';
+}
+
+function showPopup(msg) {
+  const finalmsg = popupContainer.querySelector('#final-message');
+
+  popupContainer.style.display = 'flex';
+  finalmsg.innerHTML = msg;
+}
+
+function cleanUI() {
+  hidePopup();
+  myhead.style.display = 'none';
+  mylines.forEach((line) => (line.style.display = 'none'));
+  wordContainer.innerHTML = '';
+  wrongLetterContainer.innerHTML = '';
+}
+
 function setLetters(word) {
   const arrayWord = [...word];
 
-  arrayWord.forEach((letter) => {
+  //each letter create a <span>
+  arrayWord.forEach(() => {
     const newSpan = document.createElement('span');
     newSpan.classList.add('letter');
     wordContainer.appendChild(newSpan);
@@ -87,6 +158,7 @@ async function getRandomWord() {
 
     const data = await res.json();
     myWord = data[0];
+    return myWord;
   } catch (err) {
     console.log(err);
   }
