bd16a54f3b3eaf537fc62d970f7808d135b3ccfb
Guillermoo <guillermo.cano.es@gmail.com>
2022-03-30 19:11:57 +0200

    music-player click slider

--- music-player/script.js
+++ music-player/script.js
@@ -1,41 +1,99 @@
 'use strict';
 
 const audioPlayer = document.getElementById('audio');
-const backBtn = document.getElementById('prev');
-//const iconBtn = document.querySelector('.fas');
+const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
 const musicContainer = document.querySelector('.music-container');
 const progressEl = document.getElementById('progress');
+const progressCont = document.getElementById('progress-container');
 const titleEl = document.getElementById('title');
 
-playBtn.addEventListener('click', managePlayer);
+const songs = ['hey', 'summer', 'ukulele'];
+let indexSong = 2;
 
-function managePlayer(e) {
-  console.log(e.target.className);
-  console.log(e.target.querySelector('.fas'));
-  e.target.classList.contains('fa-play') || e.target.classList.contains('play')
-    ? playSong(e.target)
-    : pauseSong(e.target);
+playBtn.addEventListener('click', managePlay);
+nextBtn.addEventListener('click', manageSwitch);
+prevBtn.addEventListener('click', manageSwitch);
+
+audioPlayer.addEventListener('timeupdate', manageSlider, false);
+progressCont.addEventListener('click', clickSlider, false);
+
+// Gets audio file duration
+audioPlayer.addEventListener(
+  'canplaythrough',
+  function () {
+    progressEl.max = audioPlayer.duration;
+  },
+  false
+);
+
+function clickSlider(e) {
+  console.log(e);
+  console.log(e.clientWidth);
+  console.log(this.clientWidth);
+  console.log(progressEl.value);
+
+  //console.log(progressEl.style.width);
+  //console.log(progressEl.style.width.replace('px', ''));
+  // audioPlayer.currentTime = progressEl.style.width.replace('px', '');
 }
 
-function playSong() {
+function manageSlider() {
+  progressEl.style.width = `${audioPlayer.currentTime}px`;
+}
+
+function managePlay() {
+  playBtn.querySelector('.fas').classList.contains('fa-play')
+    ? playSong(playBtn.querySelector('.fas'))
+    : pauseSong(playBtn.querySelector('.fas'));
+}
+
+function manageSwitch(e) {
+  e.currentTarget.querySelector('.fas').classList.contains('fa-forward')
+    ? nextIndex()
+    : prevIndex();
+
+  audioPlayer.src = `music/${songs[indexSong]}.mp3`;
+  playSong(playBtn.querySelector('.fas'));
+}
+
+function nextIndex() {
+  indexSong === songs.length - 1 ? (indexSong = 0) : indexSong++;
+}
+
+function prevIndex() {
+  indexSong === 0 ? (indexSong = songs.length - 1) : indexSong--;
+}
+
+function playSong(iconBtn) {
   audioPlayer.play();
 
-  console.log(iconBtn);
   iconBtn.classList.remove('fa-play');
   iconBtn.classList.add('fa-pause');
 
-  showSong();
+  showSong(audioPlayer.src);
+  manageSlider();
 }
 
-function showSong() {
-  titleEl.innerHTML = '';
-  console.log(audioPlayer.name);
+function showSong(audioSrc) {
+  musicContainer.classList.add('play');
+
+  //last element from array
+  let nameSong = audioSrc.split('/').reverse()[0].replace('.mp3', '');
+
+  titleEl.innerHTML = nameSong;
 }
 
 function pauseSong(icon) {
   audioPlayer.pause();
+  musicContainer.classList.remove('play');
   icon.classList.add('fa-play');
   icon.classList.remove('fa-pause');
 }
+
+function init() {
+  audioPlayer.src = `music/${songs[indexSong]}.mp3`;
+}
+
+init();
