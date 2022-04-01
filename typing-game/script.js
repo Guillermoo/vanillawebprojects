fc7e9808f9eb8143b80b371432973c88d35e275f
Guillermoo <guillermo.cano.es@gmail.com>
2022-04-01 19:08:51 +0200

    typegame change difficulty

--- typing-game/script.js
+++ typing-game/script.js
@@ -1,136 +1,95 @@
-const word = document.getElementById('word');
-const text = document.getElementById('text');
+'use strict';
+
+const wordToType = document.getElementById('word');
+const textEl = document.getElementById('text');
 const scoreEl = document.getElementById('score');
 const timeEl = document.getElementById('time');
-const endgameEl = document.getElementById('end-game-container');
-const settingsBtn = document.getElementById('settings-btn');
-const settings = document.getElementById('settings');
-const settingsForm = document.getElementById('settings-form');
-const difficultySelect = document.getElementById('difficulty');
+const endGameCont = document.getElementById('end-game-container');
+const selectoEl = document.getElementById('difficulty');
 
-// List of words for game
-const words = [
-  'sigh',
-  'tense',
-  'airplane',
-  'ball',
-  'pies',
-  'juice',
-  'warlike',
-  'bad',
-  'north',
-  'dependent',
-  'steer',
-  'silver',
-  'highfalutin',
-  'superficial',
-  'quince',
-  'eight',
-  'feeble',
-  'admit',
-  'drag',
-  'loving'
-];
+const values = {
+  easy: 15,
+  medium: 10,
+  difficult: 5,
+};
 
-// Init word
-let randomWord;
+let secondsLeft = 10;
 
-// Init score
-let score = 0;
+let myInterval;
+//let score = 0;
 
-// Init time
-let time = 10;
+textEl.addEventListener('keyup', checkWord);
 
-// Set difficulty to value in ls or medium
-let difficulty =
-  localStorage.getItem('difficulty') !== null
-    ? localStorage.getItem('difficulty')
-    : 'medium';
+function rightWord() {
+  scoreEl.innerText++;
+  textEl.value = '';
 
-// Set difficulty select value
-difficultySelect.value =
-  localStorage.getItem('difficulty') !== null
-    ? localStorage.getItem('difficulty')
-    : 'medium';
-
-// Focus on text on start
-text.focus();
-
-// Start counting down
-const timeInterval = setInterval(updateTime, 1000);
-
-// Generate random word from array
-function getRandomWord() {
-  return words[Math.floor(Math.random() * words.length)];
+  startNewGame();
 }
 
-// Add word to DOM
-function addWordToDOM() {
-  randomWord = getRandomWord();
-  word.innerHTML = randomWord;
+function wrongWord() {
+  score--;
 }
 
-// Update score
-function updateScore() {
-  score++;
-  scoreEl.innerHTML = score;
+function checkWord(e) {
+  e.preventDefault();
+  textEl.value === wordToType.innerText ? rightWord() : '';
 }
 
-// Update time
-function updateTime() {
-  time--;
-  timeEl.innerHTML = time + 's';
+async function getRandomWord() {
+  const res = await fetch(
+    `https://random-word-api.herokuapp.com/word?number=1`
+  );
 
-  if (time === 0) {
-    clearInterval(timeInterval);
-    // end game
-    gameOver();
-  }
+  const data = await res.json();
+  return data;
 }
 
-// Game over, show end screen
-function gameOver() {
-  endgameEl.innerHTML = `
-    <h1>Time ran out</h1>
-    <p>Your final score is ${score}</p>
-    <button onclick="location.reload()">Reload</button>
-  `;
-
-  endgameEl.style.display = 'flex';
+function showWord(word) {
+  wordToType.innerText = word;
 }
 
-addWordToDOM();
+function showLoseMessager() {
+  const markUpMessage = `
+        <h1>Time ran out</h1>
+        <p>Your final score is ${scoreEl.innerText}</p>
+        <button onclick="location.reload()">Reload</button>
+    `;
 
-// Event listeners
+  endGameCont.innerHTML = markUpMessage;
+  endGameCont.style.display = 'flex';
+}
 
-// Typing
-text.addEventListener('input', e => {
-  const insertedText = e.target.value;
+function gameLost() {
+  showLoseMessager();
+  clearInterval(myInterval);
+}
 
-  if (insertedText === randomWord) {
-    addWordToDOM();
-    updateScore();
+function updateTimer() {
+  timeEl.innerText = `${secondsLeft}s`;
+  secondsLeft === 0 ? gameLost() : '';
+  secondsLeft--;
+}
 
-    // Clear
-    e.target.value = '';
+function resetTimer() {
+  //console.log();
+  secondsLeft = values[selectoEl.value];
+  myInterval = setInterval(updateTimer, 1000);
+}
 
-    if (difficulty === 'hard') {
-      time += 2;
-    } else if (difficulty === 'medium') {
-      time += 3;
-    } else {
-      time += 5;
-    }
+async function startNewGame() {
+  clearInterval(myInterval);
+  resetTimer();
 
-    updateTime();
-  }
-});
+  const word = await getRandomWord();
+  showWord(word);
+}
 
-// Settings btn click
-settingsBtn.addEventListener('click', () => settings.classList.toggle('hide'));
+function init() {
+  startNewGame();
 
-// Settings select
-settingsForm.addEventListener('change', e => {
-  difficulty = e.target.value;
-  localStorage.setItem('difficulty', difficulty);
-});
+  textEl.value = '';
+  scoreEl.innerHTML = 0;
+}
+
+init();
