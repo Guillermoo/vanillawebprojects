a350993c916f16d76fc216a23000f54455733387
Guillermoo <guillermo.cano.es@gmail.com>
2022-04-05 17:25:31 +0200

    speechText final

--- speech-text-reader/script.js
+++ speech-text-reader/script.js
@@ -1,142 +1,148 @@
-const main = document.querySelector('main');
-const voicesSelect = document.getElementById('voices');
-const textarea = document.getElementById('text');
-const readBtn = document.getElementById('read');
+const mainContainer = document.getElementsByTagName('main');
+const textBoxContainer = document.getElementById('text-box');
 const toggleBtn = document.getElementById('toggle');
 const closeBtn = document.getElementById('close');
+const voicesSelector = document.getElementById('voices');
+const readBtn = document.getElementById('read');
+const textField = document.getElementById('text');
+
+let synth = window.speechSynthesis;
+let voices = synth.getVoices();
+let utterThis = new SpeechSynthesisUtterance();
 
 const data = [
   {
     image: './img/drink.jpg',
-    text: "I'm Thirsty"
+    text: "I'm Thirsty",
   },
   {
     image: './img/food.jpg',
-    text: "I'm Hungry"
+    text: "I'm Hungry",
   },
   {
     image: './img/tired.jpg',
-    text: "I'm Tired"
+    text: "I'm Tired",
   },
   {
     image: './img/hurt.jpg',
-    text: "I'm Hurt"
+    text: "I'm Hurt",
   },
   {
     image: './img/happy.jpg',
-    text: "I'm Happy"
+    text: "I'm Happy",
   },
   {
     image: './img/angry.jpg',
-    text: "I'm Angry"
+    text: "I'm Angry",
   },
   {
     image: './img/sad.jpg',
-    text: "I'm Sad"
+    text: "I'm Sad",
   },
   {
     image: './img/scared.jpg',
-    text: "I'm Scared"
+    text: "I'm Scared",
   },
   {
     image: './img/outside.jpg',
-    text: 'I Want To Go Outside'
+    text: 'I Want To Go Outside',
   },
   {
     image: './img/home.jpg',
-    text: 'I Want To Go Home'
+    text: 'I Want To Go Home',
   },
   {
     image: './img/school.jpg',
-    text: 'I Want To Go To School'
+    text: 'I Want To Go To School',
   },
   {
     image: './img/grandma.jpg',
-    text: 'I Want To Go To Grandmas'
-  }
+    text: 'I Want To Go To Grandmas',
+  },
 ];
 
-data.forEach(createBox);
+toggleBtn.addEventListener('click', showToggleTextBox);
+closeBtn.addEventListener('click', showToggleTextBox);
+readBtn.addEventListener('click', readSentence);
+voicesSelector.addEventListener('change', setVoice);
 
-// Create speech boxes
-function createBox(item) {
-  const box = document.createElement('div');
-
-  const { image, text } = item;
-
-  box.classList.add('box');
-
-  box.innerHTML = `
-    <img src="${image}" alt="${text}" />
-    <p class="info">${text}</p>
-  `;
-
-  box.addEventListener('click', () => {
-    setTextMessage(text);
-    speakText();
-
-    // Add active effect
-    box.classList.add('active');
-    setTimeout(() => box.classList.remove('active'), 800);
-  });
-
-  main.appendChild(box);
-}
-
-// Init speech synth
-const message = new SpeechSynthesisUtterance();
-
-// Store voices
-let voices = [];
-
-function getVoices() {
-  voices = speechSynthesis.getVoices();
-
-  voices.forEach(voice => {
-    const option = document.createElement('option');
-
-    option.value = voice.name;
-    option.innerText = `${voice.name} ${voice.lang}`;
-
-    voicesSelect.appendChild(option);
-  });
-}
-
-// Set text
-function setTextMessage(text) {
-  message.text = text;
-}
-
-// Speak text
-function speakText() {
-  speechSynthesis.speak(message);
-}
-
-// Set voice
 function setVoice(e) {
-  message.voice = voices.find(voice => voice.name === e.target.value);
+  console.log(e.target);
+  utterThis.voice = voices[e.target.selectedIndex];
 }
 
-// Voices changed
-speechSynthesis.addEventListener('voiceschanged', getVoices);
+function showToggleTextBox() {
+  textField.value = '';
+  textBoxContainer.classList.toggle('show');
+}
 
-// Toggle text box
-toggleBtn.addEventListener('click', () =>
-  document.getElementById('text-box').classList.toggle('show')
-);
+function readSentence(e) {
+  e.preventDefault();
 
-// Close button
-closeBtn.addEventListener('click', () =>
-  document.getElementById('text-box').classList.remove('show')
-);
+  saySentence(textField.value, voicesSelector.selectedIndex);
+}
 
-// Change voice
-voicesSelect.addEventListener('change', setVoice);
+const checkBrowserCompatibility = () => {
+  'speechSynthesis' in window
+    ? console.log('Web Speech API supported!')
+    : console.log('Web Speech API not supported :-(');
+};
 
-// Read text button
-readBtn.addEventListener('click', () => {
-  setTextMessage(textarea.value);
-  speakText();
-});
+/**
+ * sentence to speech, index language in voices
+ * @param {*} sentence
+ * @param {*} index
+ */
+function saySentence(sentence, index) {
+  utterThis.text = sentence;
+  if (index) utterThis.voice = voices[index];
+  synth.speak(utterThis);
+}
 
-getVoices();
+/**
+ * Print all boxes from data
+ */
+function createBoxActions(mydata) {
+  mydata.map((action) => {
+    const boxCont = document.createElement('div');
+    boxCont.innerHTML = `
+    <img src="${action.image}" alt="${action.text}">
+    `;
+
+    const infoCont = document.createElement('p');
+    infoCont.classList.add('info');
+    infoCont.innerText = action.text;
+    boxCont.appendChild(infoCont);
+    boxCont.classList.add('box');
+
+    // boxCont.addEventListener('click', speechText);
+    boxCont.addEventListener('click', () => {
+      saySentence(action.text, voicesSelector.selectedIndex);
+      boxCont.classList.add('active');
+      setTimeout(() => {
+        boxCont.classList.remove('active');
+      }, 800);
+    });
+    mainContainer[0].insertAdjacentElement('beforeend', boxCont);
+  });
+}
+
+/** Load voices on selector element */
+function loadVoices() {
+  //console.log(voices);
+  voices.map((voice) => {
+    const option = document.createElement('option');
+    //console.log(voice);
+    option.text = voice.name + ' ' + voice.lang;
+    option.value = voice.name;
+    voicesSelector.appendChild(option);
+  });
+}
+
+function init() {
+  checkBrowserCompatibility();
+  createBoxActions(data);
+  loadVoices();
+}
+
+init();
